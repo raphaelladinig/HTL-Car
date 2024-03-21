c803606f11e4a1fdf2f45d8b509ce3d2fa5ca938
raphaelladinig <raphaelladinig07@gmail.com>
2024-03-21 10:03:50 +0100

    add .clang format

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,27 @@ struct Motor {
 const Motor right = {2, 3};
 const Motor left = {4, 5};
 
+void forward(Motor motor);
+
 void setup() {
     pinMode(right.in, OUTPUT);
     pinMode(right.out, OUTPUT);
     pinMode(left.in, OUTPUT);
     pinMode(left.out, OUTPUT);
-    
+
     Serial.begin(9600);
     Serial.println("ON");
 }
 
 void loop() {
-    analogWrite(right.in, 255);
-    analogWrite(right.out, 0);
+    forward(right);
+    delay(5000);
+    forward(left);
+}
+
+//---------------------------------------------------------------------------------
+
+void forward(Motor motor) {
+    analogWrite(motor.in, 255);
+    analogWrite(motor.out, 0);
 }
