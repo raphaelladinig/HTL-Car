6d110ef65dd31828523082e13dc913af31f706a7
raphaelladinig <raphaelladinig07@gmail.com>
2024-03-21 14:43:14 +0100

    own lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,71 @@
-#include "FahrzeugLib.h"
 #include <Arduino.h>
 
 struct Motor {
-    int pin;
+    int noPwm;
     int pwm;
+
+    void motorControl(int speed) {
+        if (speed > 0) {
+            digitalWrite(noPwm, 0);
+            analogWrite(pwm, speed);
+        } else {
+            digitalWrite(noPwm, 1);
+            analogWrite(pwm, (255 + speed));
+        }
+    }
+
+    void stop() {
+        digitalWrite(noPwm, 0);
+        digitalWrite(pwm, 0);
+    }
+};
+
+struct Car {
+    Motor right;
+    Motor left;
+
+    void move(int speed) {
+        right.motorControl(speed);
+        left.motorControl(speed);
+        Serial.print("Forward: ");
+        Serial.println(speed);
+    }
+
+    void rotate(int speed) {
+        Serial.print("Rotate: ");
+        right.motorControl(speed);
+        left.motorControl(-speed);
+        Serial.println(speed);
+    }
+
+    void stop() {
+        right.stop();
+        left.stop();
+        Serial.println("Stop");
+    }
+
+    void curve(int speed, double form) {
+        if (form > 0) {
+            right.motorControl(speed);
+            left.motorControl(speed / pow(2, form));
+        } else {
+            right.motorControl(speed / pow(2, form));
+            left.motorControl(speed);
+        }
+    } 
 };
 
 // Pin 3 and 5 are PWM capable
-const Motor right = {2, 3};
-const Motor left = {4, 5};
+Car car = {{2, 3}, {4, 5}};
 int speed = 0;
-
-void motorControl(Motor motor, int speed);
-void straight(int speed);
-void rotate(int speed);
 
 //---------------------------------------------------------------------------------
 
 void setup() {
-    pinMode(right.pin, OUTPUT);
-    pinMode(right.pwm, OUTPUT);
-    pinMode(left.pin, OUTPUT);
-    pinMode(left.pwm, OUTPUT);
+    pinMode(car.right.noPwm, OUTPUT);
+    pinMode(car.right.pwm, OUTPUT);
+    pinMode(car.left.noPwm, OUTPUT);
+    pinMode(car.left.pwm, OUTPUT);
     
     speed = 255;
 
@@ -30,34 +74,11 @@ void setup() {
 }
 
 void loop() {
-    straight(speed);
-    delay(5000);
-    straight(-speed);
-    delay(5000);
-}
-
-//---------------------------------------------------------------------------------
-
-void motorControl(Motor motor, int speed) {
-    if (speed > 0) {
-        digitalWrite(motor.pin, 0);
-        analogWrite(motor.pwm, speed);
-    } else {
-        digitalWrite(motor.pin, 1);
-        analogWrite(motor.pwm, (255 + speed));
-    }
-}
-
-void straight(int speed) {
-    motorControl(right, speed);
-    motorControl(left, speed);
-    Serial.print("Forward: ");
-    Serial.println(speed);
-}
-
-void rotate(int speed) {
-    Serial.print("Rotate: ");
-    motorControl(right, speed);
-    motorControl(left, -speed);
-    Serial.println(speed);
+    speed = 100;
+    car.move(speed);
+    delay(2000);
+    car.curve(speed, -1);
+    delay(2000);
+    car.curve(speed, 1);
+    delay(2000);
 }
