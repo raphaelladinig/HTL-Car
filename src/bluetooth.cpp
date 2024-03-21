26295f8eae1b5cc787d1d2948843229332867eff
Peter <peteryt690@gmail.com>
2024-03-21 15:29:41 +0100

    zwischenspeicher

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1,37 +1,36 @@
 #include <bluetooth.h>
 
+char btChar = ' ';
+char btId = ' ';
+char btIdTemp = ' ';
+bool btIdExpected = true;
+bool btNewReading = false;
+String btValueString = "";
+float btValue;
+
 Value btQuery(SoftwareSerial bt) {
+   Value result;
 
+    btNewReading = false;
+    if (bt.available()) {
+        btChar = bt.read();
+        Serial.write(btChar);
+        if (btIdExpected) {
+            btIdTemp = btChar;
+            btIdExpected = false;
+            btValueString = "";
+        } else {
+            if ((btChar != ' ') && (btChar != '\n'))
+                btValueString += btChar;
+            if (btChar == '\n') {
+                btId = btIdTemp;
+                btValue = btValueString.toFloat();
+                btValueString = "";
+                btIdExpected = true;
+                btNewReading = true;
+            }
+        }
+    }
+ 
+   return result;
 }
-
-// SoftwareSerial bt(10, 11);
-// char btChar = ' ';
-// char btId = ' ';
-// char btIdTemp = ' ';
-// bool btIdExpected = true;
-// bool btNewReading = false;
-// String btValueString = "";
-// float btValue;
-//
-// void btQuery() {
-//     btNewReading = false;
-//     if (bt.available()) {
-//         btChar = bt.read();
-//         Serial.write(btChar);
-//         if (btIdExpected) {
-//             btIdTemp = btChar;
-//             btIdExpected = false;
-//             btValueString = "";
-//         } else {
-//             if ((btChar != ' ') && (btChar != '\n'))
-//                 btValueString += btChar;
-//             if (btChar == '\n') {
-//                 btId = btIdTemp;
-//                 btValue = btValueString.toFloat();
-//                 btValueString = "";
-//                 btIdExpected = true;
-//                 btNewReading = true;
-//             }
-//         }
-//     }
-// }
