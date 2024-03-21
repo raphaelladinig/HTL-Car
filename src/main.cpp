7e1de22560cc840e9192561584491aa3ae19e374
raphaelladinig <raphaelladinig07@gmail.com>
2024-03-21 09:49:16 +0100

    commit progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+struct Motor {
+    int in;
+    int out;
+};
+
+const Motor right = {2, 3};
+const Motor left = {4, 5};
+
 void setup() {
+    pinMode(right.in, OUTPUT);
+    pinMode(right.out, OUTPUT);
+    pinMode(left.in, OUTPUT);
+    pinMode(left.out, OUTPUT);
     
+    Serial.begin(9600);
+    Serial.println("ON");
 }
 
 void loop() {
-    
+    analogWrite(right.in, 255);
+    analogWrite(right.out, 0);
 }
